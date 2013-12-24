f34773f4ae5e8ab3ad067e5e9fd57bb4996a2186
Ross Duggan <rduggan@engineyard.com>
2013-12-24 00:07:21 +0000

    Bump version to 0.1.2
    - Short, plaintext responses for documented errors on the command line
    - Fix doubled error output for `pontoon droplet backups`
    - Fix bugs breaking interactive configuration under root accounts, or
      where `command` is only a builtin.

--- pontoon/__init__.py
+++ pontoon/__init__.py
@@ -2,7 +2,7 @@
 
 __name__ = 'pontoon'
 __description__ = 'A Python CLI for Digital Ocean'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Ross Duggan'
 __author_email__ = 'ross.duggan@acm.org'
 __url__ = 'https://github.com/duggan/pontoon'
