e6929784c8672d826579d2ad41c036d2ad70aceb
Ross Duggan <ross@barricade.io>
2015-10-13 13:53:44 +0100

    Fix import in docs

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 import pontoon
-from pontoon import __version__
+from pontoon.meta import __version__
 
 # -- General configuration -----------------------------------------------------
 
